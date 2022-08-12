8d6196499a9210dc46a7be45906140798004e146
harsit09 <84085808+harsit09@users.noreply.github.com>
2022-08-12 21:51:59 +0530

    Update heapsort.c
    HEAPSORT USING RECURSION

--- heapsort.c
+++ heapsort.c
@@ -1,28 +1,35 @@
 #include<stdio.h>
+#include<time.h>
+//to arrange in heap
 void max_heapify(int arr[], int n, int i) 
 {
     int par, left, right,tmp;
+    // largest element will be the root
     par= i;
+    // the element at this position is left child
     left =2*i +1;
+    // this is right child
     right=2*i+2;
 
+    // if the left child is larger than root
     if (left < n && arr[left] > arr[par])
     {
         /* code */
         par =left;  
     }
-    
+     // if rt is larger
     if (right < n && arr[right] > arr[par])
     {
         par = right;
     }
-
+    // if largest is not the root
     if (par !=i) 
     {
         /* code */
         tmp = arr[i];
         arr[i] = arr[par];
         arr[par]= tmp;
+        // recursive call to arrange the effected tree again
         max_heapify(arr, n , par);
     
     }
@@ -32,16 +39,19 @@ void max_heapify(int arr[], int n, int i)
 
 void heapsort (int arr[], int n) {
     int i , tmp;
+    // ceateing heap
     for ( i = n / 2 - 1; i >= 0; i--)
     
         /* code */
         max_heapify(arr,n,i);
+        // perfoem haeapsot 1 by 1
     for(i=n-1;i>=0;i--) 
     {
+        // moving current root to end
             tmp=arr[0];
             arr[0]=arr[i];
             arr[i]=tmp;
-
+            // rearranging the elements foe reduced heap
             max_heapify(arr,i,0);
 
     }
@@ -52,6 +62,7 @@ void heapsort (int arr[], int n) {
 int main() 
 {
     int arr[20] ,n ,i;
+    double start,end;
     printf("\n enter the number of elements");
     scanf("%d",&n);
 
@@ -61,14 +72,24 @@ int main()
      scanf("\n %d",&arr[i]);
         
     }
+    printf("\n the unsorted array is ");
+    for ( i = 0; i < n; i++)
+    {
+        printf(" %d",arr[i]);
+        
+    }
+    
+    start=clock();
     heapsort(arr,n);
     printf("\n the sorted array is : ");
     for ( i = 0; i < n; i++)
     {
         /* code */
-        printf("\n %d", arr[i]);
+        printf(" %d", arr[i]);
 
     }
+    end= clock();
+    printf("\n %lf SEC",(end-start)/CLOCKS_PER_SEC);
     
 return 0;
 }
